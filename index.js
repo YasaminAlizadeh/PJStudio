88974596a69428be3c51848c9b444cbde3eb1ad9
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2022-03-02 04:14:46 +0330

    Added Hover and Click Functionallity

---
+++ index.js
@@ -0,0 +1,42 @@
+const hoverAnimation = (items) => {
+  items.forEach((item) =>
+    item.addEventListener("mouseover", () => {
+      items.forEach(
+        (element) => element !== item && element.classList.add("item--inactive")
+      );
+    })
+  );
+
+  items.forEach((item) =>
+    item.addEventListener("mouseout", () => {
+      items.forEach(
+        (element) =>
+          element !== item && element.classList.remove("item--inactive")
+      );
+    })
+  );
+};
+
+const process_items = [...document.getElementsByClassName("process__item")];
+hoverAnimation(process_items);
+
+process_items.forEach((item) =>
+  item.addEventListener("click", () => {
+    process_items.forEach((element) =>
+      element.classList.remove("process__item--active")
+    );
+    item.classList.add("process__item--active");
+  })
+);
+
+const offer_items = [...document.getElementsByClassName("options__item")];
+hoverAnimation(offer_items);
+
+offer_items.forEach((item) =>
+  item.addEventListener("click", () => {
+    offer_items.forEach((element) =>
+      element.classList.remove("options__item--active")
+    );
+    item.classList.add("options__item--active");
+  })
+);
