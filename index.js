ead1802ae8dd69998750ccb8ef0c382478b1e0f3
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2022-03-27 07:20:45 +0430

    feat(header):  menu functionality  in small sizes

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 document.body.ondragstart = () => {
   return false;
 };
+
+const menu = document.getElementById("menu");
+const menuOpenBtn = document.getElementById("menu-open-btn");
+const menuCloseBtn = document.getElementById("menu-close-btn");
+
+menuOpenBtn.addEventListener("click", () => {
+  menu.classList.add("menu--open");
+  menu.style.transition = "all 0.3s ease-in-out";
+});
+
+menuCloseBtn.addEventListener("click", () => {
+  menu.classList.remove("menu--open");
+});
+
+window.addEventListener("resize", () => {
+  menu.classList.remove("menu--open");
+  menu.style.transition = "none";
+});
+
 const hoverAnimation = (items) => {
   items.forEach((item) =>
     item.addEventListener("mouseover", () => {
